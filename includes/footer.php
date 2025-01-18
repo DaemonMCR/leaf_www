0cd07ea0b2074c648de5aba529caa255b4891564
Thomas Turner <thomas_turner@talktalk.net>
2025-01-18 21:05:32 +0000

    www: Add Leaf logo to footer bar

--- includes/footer.php
+++ includes/footer.php
@@ -4,4 +4,11 @@
     <p>Open-source, AOSP-based Android custom ROM</p>
     <p>© 2022-2025 The LeafOS Project</p>
   </div>
+
+  <div class="footer-logo">
+        <a href="index.php">
+            <img src=".././content/images/devices/leaf_gsi_arm64.webp" alt="Logo" class="footer-logo-image">
+        </a>
+    </div>
+
 </footer>
